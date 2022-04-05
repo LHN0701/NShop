3ff7c6c6f26b374e6e4aad86d0ef013f67e5817c
NAM LY <lyhoangnam0701@gmail.com>
2022-04-05 10:50:20 +0700

    add cookie authentication without identity

---
+++ NShop.AdminApp/Services/IUserApiClient.cs
@@ -0,0 +1,13 @@
+﻿using NShop.ViewModels.System.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NShop.AdminApp.Services
+{
+    public interface IUserApiClient
+    {
+        Task<string> Authenticate(LoginRequest request);
+    }
+}
