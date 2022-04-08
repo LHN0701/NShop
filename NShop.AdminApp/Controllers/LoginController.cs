4e43e6f5ac0e7e1dc571a25a4cdb60fd2cecf5a9
NAM LY <lyhoangnam0701@gmail.com>
2022-04-08 22:40:46 +0700

    user list filter

--- NShop.AdminApp/Controllers/LoginController.cs
+++ NShop.AdminApp/Controllers/LoginController.cs
@@ -42,6 +42,11 @@ namespace NShop.AdminApp.Controllers
                 return View(ModelState);
 
             var result = await _userApiClient.Authenticate(request);
+            if (result.ResultObj == null)
+            {
+                ModelState.AddModelError("", result.Message);
+                return View();
+            }
 
             var userPrincipal = this.ValidateToken(result.ResultObj);
 
